b8682f3320fa881ca2200746d8a1b982f420f1d4
Aaron Knol <aaron.knol@gmail.com>
2022-03-07 17:15:13 +1100

    Fix durationInSeconds resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -45,10 +45,10 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
 
-    durationInSeconds: ({ durationInSeconds }) => durationInSeconds,
+    durationInSeconds: ({ length }) => length,
   },
   Module: {
-    durationInSeconds: ({ durationInSeconds }) => durationInSeconds,
+    durationInSeconds: ({ length }) => length,
   },
 };
 
